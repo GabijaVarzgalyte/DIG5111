210683e831366bf3f56f0538b1d39c4b12303a9b
GabijaVarzgalyte <gabbyvone@gmail.com>
2025-03-24 17:00:23 +0000

    creating functions

--- Applied Coding Task/untitled.m
+++ Applied Coding Task/untitled.m
@@ -1,1 +1,9 @@
-%test
+%Write a MATLAB script/function which will allow the user to compare the frequency content of two audio files. 
+
+%audioread() %% to read audio files
+%audiowrite() %% to save processed audio
+%normalize() %% normalising integers between -1 and 1
+%fft() %% fast fourier transform for freq analysis
+%plot() %%plotting graphs function
+%conv() %%to apply convolution
+%filter() %%applying a digital filter
